68e19878c1cd0d5ed6c5724c376e67be756fb443
Kumsal Obuz <iam@denizkumsal.com>
2017-06-15 22:24:00 -0400

    adds conditional layout to display all possible sections

--- src/Section.js
+++ src/Section.js
@@ -1,20 +1,30 @@
 import Description from './Description';
 import React from 'react';
+import ReactSVG from 'react-svg';
 import Summary from './Summary';
 import Tech from './Tech';
 import Title from './Title';
 
 const Section = (props) => {
+    const data = props.data
+    let secondary = null
+    if (data.url)
+        secondary = <a target="_blank" href={ data.url }>
+                      <ReactSVG path="external-link.svg" style={ { width: 24 } } />
+                    </a>
+    if (data.date)
+        secondary = data.date
+    if (data.location)
+        secondary = `${data.location} - ${data.date}`
+    if (data.role)
+        secondary = data.role
+
     return (
         <section className="section">
-            <Title primary={props.project.title} secondary={<a href={props.project.url}><i className="material-icons">open_in_new</i></a>} />
-            {props.project.tech && <Tech skills={props.project.tech} indicator=">" />}
-            {props.project.summary && <Summary entries={props.project.summary} />}
-            {props.project.description && <Description entries={props.project.description} />}
-{/*
-            {props
-                .entries
-                .map((entry, index) => <p key={index}>{entry}</p>)}*/}
+          <Title primary={ data.title } secondary={ secondary } />
+          { data.tech && <Tech skills={ data.tech } indicator=">" /> }
+          { data.summary && <Summary entries={ data.summary } /> }
+          { data.description && <Description entries={ data.description } /> }
         </section>
     )
 }
