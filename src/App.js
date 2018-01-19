5638ce1956bcc216944127299042766e2ea54e46
Kumsal Obuz <iam@denizkumsal.com>
2018-01-19 17:08:33 -0500

    Adds podcasts and meetups

--- src/App.js
+++ src/App.js
@@ -1,69 +1,81 @@
-import './reset.css';
-import './App.css';
+import "./reset.css";
+import "./App.css";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Article from './Article';
-import Description from './Description';
-import Resume from './resume.json';
-import Section from './Section';
-import Summary from './Summary';
+import Article from "./Article";
+import Description from "./Description";
+import Resume from "./resume.json";
+import Section from "./Section";
+import Summary from "./Summary";
 
 class App extends Component {
   render() {
     return (
       <main>
         <header>
-          <h1>{ Resume.id }</h1>
+          <h1>{Resume.id}</h1>
           <ul>
             <li>
-              <a href={ `mailto:${Resume.email}` }>
-                { Resume.email }
+              <a href={`mailto:${Resume.email}`}>{Resume.email}</a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href={Resume.github}>
+                {Resume.github}
               </a>
             </li>
             <li>
-              <a target="_blank" rel="noopener noreferrer" href={ Resume.github }>
-                { Resume.github }
-              </a>
-            </li>
-            <li>
-              <a target="_blank" rel="noopener noreferrer" href={ Resume.linkedin }>
-                { Resume.linkedin }
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={Resume.linkedin}
+              >
+                {Resume.linkedin}
               </a>
             </li>
           </ul>
-          <Summary entries={ Resume.summary } />
+          <Summary entries={Resume.summary} />
         </header>
         <Article title="Highlights">
-          <Description entries={ Resume.highlights } />
+          <Description entries={Resume.highlights} />
         </Article>
         <Article title="Notable Projects">
-          { Resume
-              .notableProjects
-              .map((project, index) => <Section key={ index } data={ project }></Section>) }
+          {Resume.notableProjects.map((project, index) => (
+            <Section key={index} data={project} />
+          ))}
         </Article>
         <Article title="Experience" className="page">
-          { Resume
-              .experience
-              .map((experience, index) => <Section key={ index } data={ experience }></Section>) }
+          {Resume.experience.map((experience, index) => (
+            <Section key={index} data={experience} />
+          ))}
         </Article>
         <Article title="Education" className="page">
-          { Resume
-              .education
-              .map((education, index) => <Section key={ index } data={ education }></Section>) }
+          {Resume.education.map((education, index) => (
+            <Section key={index} data={education} />
+          ))}
         </Article>
-        <Article title="Publications">
-          { Resume
-              .publications
-              .map((publication, index) => <Section key={ index } data={ publication }></Section>) }
+        <Article title="Followed Podcasts">
+          {Resume.following.podcasts.map((podcast, index) => (
+            <Section key={index} data={podcast} />
+          ))}
+        </Article>
+        <Article title="Attended Meetups">
+          {Resume.following.meetups.map((meetup, index) => (
+            <Section key={index} data={meetup} />
+          ))}
+        </Article>
+        <Article title="Publications" className="page">
+          {Resume.publications.map((publication, index) => (
+            <Section key={index} data={publication} />
+          ))}
         </Article>
         <Article title="References">
-          { Resume
-              .references
-              .map((reference, index) => <Section key={ index } data={ reference }></Section>) }
+          {Resume.references.map((reference, index) => (
+            <Section key={index} data={reference} />
+          ))}
         </Article>
       </main>
-      );
+    );
   }
 }
 
