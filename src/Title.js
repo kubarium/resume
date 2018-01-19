1c6e7f507da36e4a63407120ffd389f1d4737621
Kumsal Obuz <iam@denizkumsal.com>
2018-01-19 17:59:18 -0500

    implements render html

--- src/Title.js
+++ src/Title.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
 
-const Title = (props) => {
-    return (
-        <h3 className="title">
-                    { props.primary }
-                    <span>{ props.secondary }</span>
-                </h3>
-    )
-}
+const Title = props => {
+  return (
+    <h3 className="title">
+      <span>{ReactHtmlParser(props.primary)}</span>
+      <span>{props.secondary}</span>
+    </h3>
+  );
+};
 
-export default Title
+export default Title;
