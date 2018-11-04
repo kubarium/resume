c20464e73d16da2cba213169404b848492c8765b
kubarium <iam@denizkumsal.com>
2018-11-04 16:16:00 -0500

    extracts styling for link opener

--- src/Section.js
+++ src/Section.js
@@ -8,23 +8,22 @@ import Title from './Title';
 const Section = (props) => {
     const data = props.data
     let secondary = null
-    if (data.url)
-        secondary = <a target="_blank" rel="noopener noreferrer" href={ data.url }>
-                      <ReactSVG path="external-link.svg" style={ { width: 24 } } />
-                    </a>
-    if (data.date)
+    if (data.url) 
+        secondary = <a target="_blank" rel="noopener noreferrer" href={data.url}>
+            <ReactSVG className="external-link" path="external-link.svg"/>
+        </a>
+    if (data.date) 
         secondary = data.date
-    if (data.location)
+    if (data.location) 
         secondary = `${data.location} - ${data.date}`
-    if (data.role)
+    if (data.role) 
         secondary = data.role
 
     return (
         <section className="section">
-          <Title primary={ data.title } secondary={ secondary } />
-          { data.tech && <Tech skills={ data.tech } indicator=">" /> }
-          { data.summary && <Summary entries={ data.summary } /> }
-          { data.description && <Description entries={ data.description } /> }
+            <Title primary={data.title} secondary={secondary}/> {data.tech && <Tech skills={data.tech} indicator="&gt;"/>}
+            {data.summary && <Summary entries={data.summary}/>}
+            {data.description && <Description entries={data.description}/>}
         </section>
     )
 }
